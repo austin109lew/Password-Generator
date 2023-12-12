631fcbbad362f0afb1e35d3199276172d7d751c4
austin109lew <150309175+austin109lew@users.noreply.github.com>
2023-12-11 19:32:06 -0500

    Update script.js from vs code

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var lengthInput = document.getElementById("length");
+var charTypeInput = document.getElementById("charType");
+var displayInput = document.getElementById("display");
+var generateBtn = document.getElementById("generate-btn");
+var copyBtn = document.getElementById("copy-btn");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", function() {
+  var length = Number(lengthInput.value);
+  var charType = charTypeInput.value;
+  var password = generatePassword(length, charType);
+  displayInput.value = password;
+});
 
-  passwordText.value = password;
+copyBtn.addEventListener("click", function() {
+  displayInput.select();
+  document.execCommand("Copy");
+  alert("Password copied to clipboard!");
+});
 
+function generatePassword(length, charType) {
+  var charSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  };
+  var charSet = charSets[charType.toLowerCase()] || charSets.lowercase;
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
